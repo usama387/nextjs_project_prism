71d30573e7785fd8a236908f57b689cfe692d543
usama387 <128306049+usama387@users.noreply.github.com>
2024-07-30 11:26:52 +0500

    Added styling sign-up route

--- src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,12 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center h-[100vh]">
+      <h3 className="font-semibold text-2xl text-blue-500">
+        Welcome to Project Prism
+      </h3>
+      <SignUp />
+    </div>
+  );
 }
