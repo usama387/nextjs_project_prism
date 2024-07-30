7a403a66d1039bb00bca747a9941d120734d68e3
usama387 <128306049+usama387@users.noreply.github.com>
2024-07-30 11:20:35 +0500

    Corrected a route from  last auth commit

--- src/app/_components/Hero.jsx
+++ src/app/_components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <a
               className="block w-full rounded bg-primary px-12 py-3 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-              href="/sign-in"
+              href="/sign-up"
             >
               Create An Account
             </a>
